fdc321868815821eaa30cf629b78f6a87edd2fb8
Maidy Cabrera <m.cabrerap@uniandes.edu.co>
2023-06-20 11:37:18 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Humanizer;
+
+Console.WriteLine("Por favor ingrese un nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su cargo");
+var cargo = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su edad");
+var edad = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Mi nombre es {nombre}, mi cargo es {cargo} y mi edad es {edad.ToWords(new System.Globalization.CultureInfo("es"))} años");
